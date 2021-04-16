906db23151fe300d9aee45b0e1d2cb343e4a171a
Zhan BingCong 詹炳聪 <bingco.zn@gmail.com>
2021-04-16 15:16:06 +0800

    分布式锁项目初步创建3

---
+++ lock/src/main/java/com/zbc/lock/LockMain.java
@@ -0,0 +1,48 @@
+package com.zbc.lock;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+
+public class LockMain {
+
+    private static int count = 1;
+
+    public static void main(String[] args) throws Exception {
+        try (CuratorFramework client = CuratorFrameworkFactory.newClient(
+                "127.0.0.1:2181",
+                60000,
+                15000,
+                new ExponentialBackoffRetry(1000, 3))
+        ) {
+            client.start();
+            List<Thread> list = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                Thread td = new Thread(() -> {
+                    try {
+                        Lock lock = new ZkLock(client, "/lock/main");
+                        lock.lock();
+                        Format formatter = new SimpleDateFormat("yyyy-MM-dd-HH-mm:ss");
+                        System.out.println(Thread.currentThread() + ": order no: " + formatter.format(new Date()) + "-" + (count++));
+                        Thread.sleep(1000L);
+                        lock.unlock();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+                td.start();
+                list.add(td);
+            }
+            for (Thread thread : list) {
+                thread.join();
+            }
+        }
+    }
+}
--- lock/src/main/java/com/zbc/lock/ZkLock.java
+++ lock/src/main/java/com/zbc/lock/ZkLock.java
@@ -1,21 +1,58 @@
 package com.zbc.lock;
 
-import java.io.Closeable;
-import java.io.IOException;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.cache.CuratorCache;
+import org.apache.curator.framework.recipes.cache.CuratorCacheListener;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
-public class ZkLock implements Lock, Closeable {
-    private String key;
+import static org.apache.curator.framework.recipes.cache.CuratorCacheListener.Type.NODE_DELETED;
 
-    public ZkLock(String key) {
-        this.key = key;
+
+public class ZkLock implements Lock {
+    private final String path;
+    private final CuratorFramework client;
+    private CountDownLatch cdl;
+    private final CuratorCache cache;
+
+    public ZkLock(CuratorFramework client, String path) throws Exception {
+        this.path = path;
+        this.client = client;
+        this.cache = CuratorCache.build(client, path);
+        this.cache.start();
     }
 
     @Override
     public void lock() {
+        boolean isLock = tryLock();
+        if (!isLock) {
+            try {
+                listen();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            lock();
+        }
+    }
 
+    private void listen() throws InterruptedException {
+        CuratorCacheListener listener = (t, o, n) -> {
+            if (t == NODE_DELETED) {
+                System.out.println(Thread.currentThread() + " --------------。>>> CuratorCache: " + t);
+                cdl.countDown();
+            }
+        };
+        cache.listenable().addListener(listener);
+        cdl.await();
+        cache.listenable().removeListener(listener);
     }
 
     @Override
@@ -25,26 +62,63 @@ public class ZkLock implements Lock, Closeable {
 
     @Override
     public boolean tryLock() {
+        this.cdl = new CountDownLatch(1);
+        try {
+            Stat stat = client.checkExists()
+                    .forPath(path);
+            if (stat == null) {
+                client.create()
+                        .creatingParentsIfNeeded()
+                        .forPath(path);
+                return true;
+            }
+        } catch (KeeperException.NodeExistsException ignore) {
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Thread.currentThread() + ":NodeExistsException ----------------。>>>");
         return false;
     }
 
     @Override
     public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-        return false;
+        boolean isLock = tryLock();
+        if (!isLock) {
+            try {
+//            this.cache = new PathChildrenCache(client, path, false);
+//            this.cache.getListenable().addListener((client, event) -> {
+//                if (event.getType() == CHILD_REMOVED) {
+//                    this.cdl.countDown();
+//                }
+//            });
+//            this.cache.start();
+                if (!this.cdl.await(time, unit)) {
+                    return false;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            isLock = tryLock(time, unit);
+        }
+        return isLock;
     }
 
     @Override
     public void unlock() {
-
+        try {
+            client.delete()
+                    .deletingChildrenIfNeeded()
+                    .forPath(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Condition newCondition() {
         return null;
-    }
-
-    @Override
-    public void close() throws IOException {
-        System.out.println("");
     }
 }

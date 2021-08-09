1ec5065b5e518e8f8cb71fd22f4cd5d211038342
Zhan BingCong 詹炳聪 <bingco.zn@gmail.com>
2021-08-09 17:31:34 +0800

    事务案例

--- cloud/eureka-consumer/src/main/java/com/xxx/consumer/EurekaConsumerApplication.java
+++ cloud/eureka-consumer/src/main/java/com/xxx/consumer/EurekaConsumerApplication.java
@@ -1,20 +1,31 @@
 package com.xxx.consumer;
 
+import com.zaxxer.hikari.HikariDataSource;
+import io.seata.rm.datasource.DataSourceProxy;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import javax.sql.DataSource;
+
 @RequestMapping
 @SpringBootApplication
 public class EurekaConsumerApplication {
+    private static ListableBeanFactory beanFactory;
+
     public static void main(String[] args) {
-        SpringApplication.run(EurekaConsumerApplication.class, args);
+        beanFactory = SpringApplication.run(EurekaConsumerApplication.class, args);
     }
 
     @Bean
@@ -29,5 +40,34 @@ public class EurekaConsumerApplication {
         ResponseEntity<String> entity = restTemplate()
                 .getForEntity("http://eureka-provider/demo", String.class);
         return entity.getBody();
+    }
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, value = "/transfer")
+    public String transfer(int fromUserId, int toUserId, double amount, String remark) {
+        JdbcTemplate jdbcTemplate = beanFactory.getBean("jdbcTemplate", JdbcTemplate.class);
+        jdbcTemplate.update("insert into t_record(amount, `from`, `to`, remark) values(?, ?, ?, ?)", new Object[]{ amount, fromUserId, toUserId, remark });
+        ResponseEntity<String> entity = restTemplate()
+                .getForEntity(String.format("http://eureka-provider/updateOrder?fromUserId=%s&toUserId=%s&amount=%s", fromUserId, toUserId, amount), String.class);
+        return entity.getBody();
+    }
+
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public DataSource dataSource() {
+        return new HikariDataSource();
+    }
+
+    @Primary
+    @Bean("dataSourceProxy")
+    public DataSourceProxy dataSourceProxy(DataSource dataSource) {
+        return new DataSourceProxy(dataSource);
+    }
+
+    @Bean("jdbcTemplate")
+    @ConditionalOnBean(DataSourceProxy.class)
+    public JdbcTemplate jdbcTemplate(DataSourceProxy dataSourceProxy) {
+        return new JdbcTemplate(dataSourceProxy);
     }
 }
--- cloud/eureka-provider/src/main/java/com/xxx/provider/EurekaProviderApplication.java
+++ cloud/eureka-provider/src/main/java/com/xxx/provider/EurekaProviderApplication.java
@@ -1,17 +1,31 @@
 package com.xxx.provider;
 
+import com.zaxxer.hikari.HikariDataSource;
+import io.seata.rm.datasource.DataSourceProxy;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Objects;
+
 @RequestMapping
 @SpringBootApplication
 public class EurekaProviderApplication {
+    private static ListableBeanFactory beanFactory;
+
     public static void main(String[] args) {
-        SpringApplication.run(EurekaProviderApplication.class, args);
-        // TODO
+        beanFactory = SpringApplication.run(EurekaProviderApplication.class, args);
         // https://github.com/seata/seata-samples/blob/master/springcloud-eureka-seata/order/src/main/java/io/seata/sample/OrderApplication.java
     }
 
@@ -19,5 +33,45 @@ public class EurekaProviderApplication {
     @RequestMapping(method = RequestMethod.GET, value = "/demo")
     public String demo() {
         return "OK";
+    }
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, value = "updateOrder")
+    public String order(int fromUserId, int toUserId, double amount) throws SQLException {
+        JdbcTemplate jdbcTemplate = beanFactory.getBean("jdbcTemplate", JdbcTemplate.class);
+        Connection connection = Objects.requireNonNull(jdbcTemplate.getDataSource())
+                .getConnection();
+        try {
+            connection.setAutoCommit(false);
+            jdbcTemplate.update("update t_card set amount = amount - ? where user_id = ?", new Object[]{amount, fromUserId});
+            jdbcTemplate.update("update t_card set amount = amount + ? where user_id = ?", new Object[]{amount, toUserId});
+            int a = 1/0;
+            connection.commit();
+        } catch (Exception e) {
+            connection.rollback();
+            e.printStackTrace();
+            return "FAIL";
+        } finally {
+            connection.close();
+        }
+        return "OK";
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public DataSource dataSource() {
+        return new HikariDataSource();
+    }
+
+    @Primary
+    @Bean("dataSourceProxy")
+    public DataSourceProxy dataSourceProxy(DataSource dataSource) {
+        return new DataSourceProxy(dataSource);
+    }
+
+    @Bean("jdbcTemplate")
+    @ConditionalOnBean(DataSourceProxy.class)
+    public JdbcTemplate jdbcTemplate(DataSourceProxy dataSourceProxy) {
+        return new JdbcTemplate(dataSourceProxy);
     }
 }
